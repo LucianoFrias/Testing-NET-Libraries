1a1398b7f16552f39b48d8d8d5f9501e41b3b887
LucianoFrias <lucianofrias1@hotmail.com>
2023-05-02 12:58:23 -0300

    Added JSON Writer

---
+++ Testing-NET-Libraries/JSON/utils/JSONWriter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Xml;
+using Testing_NET_Libraries.JSONReader.car;
+using Testing_NET_Libraries.JSONReader.utils;
+
+namespace Testing_NET_Libraries.JSON.utils
+{
+    internal class JSONWriter<T>
+    {
+        public static void writeJSONToFile(string path, T element)
+        {
+            List<T> elements = JSONReader<T>.readJson(path);
+
+            elements.Add(element);
+
+            var options = new JsonSerializerOptions { WriteIndented = true };
+            string json = JsonSerializer.Serialize(elements, options);
+            File.WriteAllText(path, json);
+
+        }
+    }
+}
--- Testing-NET-Libraries/JSONReader/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿
-using System.Text.Json;
-using Testing_NET_Libraries.JSONReader.car;
-using Testing_NET_Libraries.JSONReader.utils;
-
-
-List<Car> cars = JSONReader<Car>.readJson(@"../../../JSONReader/datafiles/cars.json");
-
-CarPrinter.printCars(cars);
