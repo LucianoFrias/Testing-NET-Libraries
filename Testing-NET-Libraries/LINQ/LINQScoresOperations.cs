dacc54effab83c4f5da2270dd098c30bb8df0c03
LucianoFrias <lucianofrias1@hotmail.com>
2023-05-02 23:13:45 -0300

    Added LINQ Operations class.

---
+++ Testing-NET-Libraries/LINQ/LINQScoresOperations.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testing_NET_Libraries.LINQ
+{
+    internal class LINQScoresOperations
+    {
+        public List<int>? Scores { get; set; }
+        public List<int>? SecondaryScores { get; set; }
+
+        public List<int>? getScoresGreatherThanFifty()
+        {
+            IEnumerable<int> scoresGreaterThanFifty =
+                from score in Scores
+                where score > 50
+                select score;
+
+            return scoresGreaterThanFifty.ToList();
+        }
+
+        public List<int>? compareSameNumbers()
+        {
+            IEnumerable<int> compareSameNumbers =
+                from score in Scores
+                join secondScore in SecondaryScores
+                on score equals secondScore
+                select score;
+
+            return compareSameNumbers.ToList();
+        }
+
+        public List<int>? orderNumbersAsc()
+        {
+            IEnumerable<int> orderScoresAsc =
+                from score in Scores
+                orderby score ascending
+                select score;
+
+            return orderScoresAsc.ToList();
+
+        }
+
+        public List<int> orderNumbersDesc()
+        {
+            IEnumerable<int> orderScoresDesc =
+                from score in Scores
+                orderby score descending
+                select score;
+
+            return orderScoresDesc.ToList();
+        }
+
+        public void checkListContents(List<int> list)
+        {
+            foreach (var element in list)
+            {
+                Console.WriteLine(element);
+            }
+
+            Console.WriteLine(" ");
+
+        }
+
+    }
+}
--- Testing-NET-Libraries/Program.cs
+++ Testing-NET-Libraries/Program.cs
@@ -4,6 +4,7 @@ using System.Text.Json;
 using Testing_NET_Libraries.JSON.utils;
 using Testing_NET_Libraries.JSONReader.car;
 using Testing_NET_Libraries.JSONReader.utils;
+using Testing_NET_Libraries.LINQ;
 
 // JSON
 
@@ -15,36 +16,27 @@ CarPrinter.printCars(cars);
 
 // LINQ
 
-List<int> scores = new List<int>() { 22, 58, 56, 50, 100, 86, 33 };
-List<int> secondScores = new List<int>() { 58, 25, 100 };
+List<int> scores = new() { 22, 58, 56, 50, 100, 86, 33 };
+List<int> secondScores = new() { 58, 25, 100 };
 
-IEnumerable<int> scoresGreaterThanFifty =
-    from score in scores
-     where score > 50
-     select score;
+LINQScoresOperations linqScoresOperations = new()
+{
+    Scores = scores,
+    SecondaryScores = secondScores
+};
 
-scoresGreaterThanFifty.ToList().ForEach(score => Console.WriteLine(score));
+List<int>? scoresGreatherThanFifty = linqScoresOperations.getScoresGreatherThanFifty();
+linqScoresOperations.checkListContents(scoresGreatherThanFifty);
 
-IEnumerable<int> compareSameNumbers =
-    from score in scores
-    join secondScore in secondScores
-    on score equals secondScore
-    select score;
+List<int>? sameNumbers = linqScoresOperations.compareSameNumbers();
+linqScoresOperations.checkListContents(sameNumbers);
 
-compareSameNumbers.ToList().ForEach((score) => Console.WriteLine(score + "\n"));
+List<int>? numbersAscending = linqScoresOperations.orderNumbersAsc();
+linqScoresOperations.checkListContents(numbersAscending);
 
-IEnumerable<int> orderScoresAsc =
-    from score in scores
-    orderby score ascending
-    select score;
+List<int>? numbersDescending = linqScoresOperations.orderNumbersDesc();
+linqScoresOperations.checkListContents(numbersDescending);
 
 
-orderScoresAsc.ToList().ForEach(score => Console.WriteLine(score));
 
-IEnumerable<int> orderScoresDesc =
-    from score in scores
-    orderby score descending
-    select score;
-
-orderScoresDesc.ToList().ForEach(score => Console.WriteLine(score));
 
