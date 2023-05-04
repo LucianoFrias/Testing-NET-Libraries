c3c629ed40b51238501767760fb233b86f58e109
LucianoFrias <lucianofrias1@hotmail.com>
2023-05-04 13:28:22 -0300

    Added Entity Framework to the project.

---
+++ Testing-NET-Libraries/Entity Framework/Data/ClientDBContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Testing_NET_Libraries.Entity_Framework.Models;
+
+namespace Testing_NET_Libraries.Entity_Framework.Data
+{
+    internal class ClientDBContext : DbContext
+    {
+        public ClientDBContext() { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=localhost;Database=clients_db;Trusted_Connection=True;TrustServerCertificate=True;");
+        } 
+
+        public DbSet<Client> Clients { get; set; }
+
+
+    }
+}
--- Testing-NET-Libraries/Program.cs
+++ Testing-NET-Libraries/Program.cs
@@ -1,6 +1,9 @@
 ﻿
+using System;
 using System.Collections;
 using System.Text.Json;
+using Testing_NET_Libraries.Entity_Framework.Data;
+using Testing_NET_Libraries.Entity_Framework.Models;
 using Testing_NET_Libraries.JSON.utils;
 using Testing_NET_Libraries.JSONReader.car;
 using Testing_NET_Libraries.JSONReader.utils;
@@ -37,6 +40,35 @@ linqScoresOperations.checkListContents(numbersAscending);
 List<int>? numbersDescending = linqScoresOperations.orderNumbersDesc();
 linqScoresOperations.checkListContents(numbersDescending);
 
+// Entity Framework
+
+
+// Instanciate our client context
+var context = new ClientDBContext();
+
+// Add Client
+//context.Add(new Client()
+//{
+//    FirstName = "Diego",
+//    LastName = "Maradona",
+//    Age = 76
+//});
+
+//context.SaveChanges();
+
+// Find Client by Id
+Client? foundClient = context.Clients.First(client => client.Id == 12);
+Console.WriteLine(foundClient.Id + " " + foundClient.FirstName);
+
+// Delete Client
+
+//context.Remove(foundClient);
+//context.SaveChanges();
+
+// Get All Clients
+
+List<Client> clients = context.Clients.ToList();
+clients.ForEach(client => Console.WriteLine(client.Id + " " + client.FirstName));
 
 
 
